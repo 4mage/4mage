b97a7cff59ea4c49d4c7110d80c358245af9e035
Abraão Marques <abmarques.fcamara@rd.com.br>
2021-05-06 01:32:47 -0300

    Working with Dependency Injection argument types in Magento 2

--- app/code/ForMage/Learning/Controller/Page/Index.php
+++ app/code/ForMage/Learning/Controller/Page/Index.php
@@ -6,24 +6,28 @@ use Magento\Framework\App\Action\Context;
 use Magento\Framework\App\ResponseInterface;
 use ForMage\Learning\Api\SizeInterface;
 use ForMage\Learning\Api\ColorInterface;
+use ForMage\Learning\Model\Product\Shoe;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
     protected $size;
     protected $color;
+    protected $product;
 
     public function __construct(
         Context $context,
         SizeInterface $size,
-        ColorInterface $color
+        ColorInterface $color,
+        Shoe $product
     ) {
         parent::__construct($context);
         $this->size = $size;
         $this->color = $color;
+        $this->product = $product;
     }
 
     public function execute()
     {
-        echo "My shirt is {$this->color->getColor()} and its size is {$this->size->getSize()}";
+        echo $this->product->getShoe();
     }
 }
