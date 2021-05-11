82d088a48deca3829347f2fd86f1c8ef2122ad53
Abraão Marques <abmarques.fcamara@rd.com.br>
2021-05-11 10:45:14 -0300

    Argument Types parte 2

--- app/code/ForMage/Learning/Controller/Page/Index.php
+++ app/code/ForMage/Learning/Controller/Page/Index.php
@@ -7,27 +7,31 @@ use Magento\Framework\App\ResponseInterface;
 use ForMage\Learning\Api\SizeInterface;
 use ForMage\Learning\Api\ColorInterface;
 use ForMage\Learning\Model\Product\Shoe;
+use ForMage\Learning\Model\Product\Shirt;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
     protected $size;
     protected $color;
     protected $product;
+    protected $shirt;
 
     public function __construct(
         Context $context,
         SizeInterface $size,
         ColorInterface $color,
-        Shoe $product
+        Shoe $product,
+        Shirt $shirt
     ) {
         parent::__construct($context);
         $this->size = $size;
         $this->color = $color;
         $this->product = $product;
+        $this->shirt = $shirt;
     }
 
     public function execute()
     {
-        echo $this->product->getShoe();
+        var_dump($this->shirt);
     }
 }
